112e90d1c35fd8ac0492ec7a6ac7dd2cc1f94f59
ambosing <ambosing_@naver.com>
2024-10-05 21:59:10 +0900

    Swagger 문서 편집

--- Welcome/main.py
+++ Welcome/main.py
@@ -7,6 +7,13 @@ app = FastAPI()
 # Path 오퍼레이션 생성. Path는 도메인명을 제외하고 / 로 시작하는 URL 부분
 # 만약 url이 https://example.com/items/foo 라면 path는 /items/foo 
 # Operation은 GET, POST, PUT/PATCH, DELETE등의 HTTP 메소드임. 
-@app.get("/")
+@app.get("/", summary="간단한 API", tags=["Simple"])
 async def root():
+    '''
+    이것은 간단한 API 입니다. 아래는 인자값입니다.
+
+    - **인자값1**은 이거고요
+    - **인자값2**는 이거입니다.
+
+    '''
     return {"message": "Hello world"}
